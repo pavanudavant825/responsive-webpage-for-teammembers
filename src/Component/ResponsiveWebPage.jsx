ac3b69a356d09082ba891025c5352eb81f470120
pavanudavant825 <pavanudavant66@gmail.com>
2024-02-20 21:34:39 +0530

    first commit

---
+++ src/Component/ResponsiveWebPage.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const ResponsiveWebPage = () => {
+  return (
+    <div className='webpage'>
+        <header>
+        <h1 className='heading1'>Veritech Software Services</h1>
+        <p className='para1'>Welcome to Veritech Software Services, where a dedicated team of professional works together to bring innovations, creativity and expertise to software industry.get to know to individuals who make our company thrive</p>
+        <hr/>
+        <h2 className='heading2'>Meet Our Team</h2>
+        </header>
+        <section>
+            <h2 className='heading3'>Leadership / Management Team</h2>
+            <div className='team1'>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Founder CEO</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Founder CEO</h4></div>
+            </div>
+            <div className='team2'>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Founder CEO</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Founder CEO</h4></div>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Founder CEO</h4></div>
+            </div>
+            <h2 className='heading4'>Development Team</h2>
+            <div className='team3'>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Team Lead SE</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Software Engineer</h4></div>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Software Engineer</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Software Engineer</h4></div> 
+            </div>
+            <div className='team4'>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Software Engineer</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Software Engineer</h4></div>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Software Engineer</h4></div>
+            </div>
+            <h2 className='heading5'>Sales and Marketing Team</h2>
+            <div className='team3'>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Team Lead</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Sales Officer</h4></div>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Sales Officer</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Sales Officer</h4></div> 
+            </div>
+            <div className='team4'>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Marketing Officer</h4></div> 
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Marketing Officer</h4></div>
+                <div className='card'><img src="./Assets/img1.png" alt="" /><h3>Founder Name</h3><h4>Marketing Officer</h4></div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default ResponsiveWebPage
